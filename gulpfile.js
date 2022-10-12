1e8c8d26da7c73b551baadfb2ec251df7d25dba6
IlyaSoGood <87233953+IlyaSoGood@users.noreply.github.com>
2022-10-12 23:26:06 +0300

    Урок 87. XMLHTTP Request переделан на Fetch API. Отправка данных с формы. Оповещение пользователя

--- gulpfile.js
+++ gulpfile.js
@@ -17,32 +17,32 @@ gulp.task("copy-html", () => {
 
 gulp.task("build-js", () => {
     return gulp.src("./src/js/main.js")
-                .pipe(webpack({
-                    mode: 'development',
-                    output: {
-                        filename: 'script.js'
-                    },
-                    watch: false,
-                    devtool: "source-map",
-                    module: {
-                        rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
-                                    debug: true,
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
-                                }]]
-                              }
-                            }
-                          }
-                        ]
-                      }
-                }))
+                // .pipe(webpack({
+                //     mode: 'development',
+                //     output: {
+                //         filename: 'script.js'
+                //     },
+                //     watch: false,
+                //     devtool: "source-map",
+                //     module: {
+                //         rules: [
+                //             {
+                //             test: /\.m?js$/,
+                //             exclude: /(node_modules|bower_components)/,
+                //             use: {
+                //                 loader: 'babel-loader',
+                //                 options: {
+                //                 presets: [['@babel/preset-env', {
+                //                     debug: true,
+                //                     corejs: 3,
+                //                     useBuiltIns: "usage"
+                //                 }]]
+                //                 }
+                //             }
+                //             }
+                //         ]
+                //         }
+                // }))
                 .pipe(gulp.dest(dist + '/js'))
                 .pipe(browsersync.stream());
 });
